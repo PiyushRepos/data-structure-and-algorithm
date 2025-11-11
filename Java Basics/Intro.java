abe24a62f6c2b6fb63b98d020a747bb809e350b1
Piyush Kumar <piyush2004.dev@gmail.com>
2025-11-12 03:27:54 +0530

    complete intro to java

---
+++ Java Basics/Intro.java
@@ -0,0 +1,52 @@
+// Java is popular programming language
+// It is owned by Oracle, and more than 3 billion devices run Java.
+
+// It is used for:
+// Mobile applications (specially Android apps)
+// Desktop applications
+// Web applications
+// Web servers and application servers
+// Games
+// Database connection
+// And much, much more!
+
+// Why Use Java?
+// Java works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc.)
+// It is one of the most popular programming languages in the world
+// It has a large demand in the current job market
+// It is easy to learn and simple to use
+// It is open-source and free
+// It is secure, fast and powerful
+// It has huge community support (tens of millions of developers)
+// Java is an object oriented language which gives a clear structure to programs and allows code to be reused, lowering development costs
+// As Java is close to C++ and C#, it makes it easy for programmers to switch to Java or vice versa
+
+// Boilerplate
+// In Java, every application begins with a class name, and that class must match the filename.
+//
+// public class Intro {
+//   public static void main(String[] args){ // main is starting point of every java program
+
+//   }
+// }
+
+// Java Example
+public class Intro {
+
+  public static void main(String[] args) {
+    String name = "Piyush";
+    System.out.println("Hello " + name) // println() method to print a line of text to the screen:
+  }
+}
+
+// Statements
+// A computer program is a set of "instructions" to be exceuted by a computer.
+// In a programming language, these programming instructions are called statements.
+
+// Example
+// System.out.println("Hello, World");
+
+// It is important that we end the statement with a semicolon ;.
+// If you forget the semicolon (;), an error will occur and the program will not run: error: ';' expected
+
+// The statements are executed, one by one, in the same order as they are written
